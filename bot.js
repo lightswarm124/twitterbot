4ba4e7a3de51ca6ba13ced51e0d6c136371622e7
lightswarm124 <lightswarm@gmail.com>
2017-12-30 00:51:04 -0500

    first commit

--- bot.js
+++ bot.js
@@ -5,36 +5,16 @@ let twit = require('twit');
 let config = require('./config.js');
 
 let Twitter = new twit(config);
-/*
-let BTCUSDPrice = cc.price('BTC', ['USD'])
-    .then(price => {
-        console.log(price);
-    })
-    .catch(console.error);
 
-let BCHUSDPrice = cc.price('BCH', ['USD'])
-    .then(price => {
-        console.log(price);
-    })
-    .catch(console.error);
-
-let BCHBTCPrice = cc.price('BCH', ['BTC'])
-    .then(price => {
-        console.log(price);
-    })
-    .catch(console.error);
-
-console.log(BTCUSDPrice, " ", BCHUSDPrice, " ", BCHBTCPrice);
-
-let BCHUSDPrice = await cc.price('BCH', ['USD']);
-let BCHBTCPrice = await cc.price('BCH', ['BTC']);
-*/
 async function FetchPrice() {
     try {
         const BTCUSDData = await cc.price('BTC', ['USD']);
         const BCHUSDData = await cc.price('BCH', ['USD']);
         const BCHBTCData = await cc.price('BCH', ['BTC']);
-        return 'BTCUSD Price: ' + BTCUSDData.USD + ' USD/BTC\n' + 'BCHUSD Price: ' + BCHUSDData.USD + ' USD/BCH\n' + 'BCHBTC Price: ' + BCHBTCData.BTC + ' BCH/BTC';
+        const ETHBTCData = await cc.price('ETH', ['BTC']);
+        const BCHETHData = await cc.price('BCH', ['ETH']);
+        const USDTUSDData = await cc.price('USDT', ['USD']);
+        return 'USDTUSD Price: ' + USDTUSDData.USD + 'USDT/USD\n' + 'BTCUSD Price: ' + BTCUSDData.USD + ' USD/BTC\n' + 'BCHUSD Price: ' + BCHUSDData.USD + ' USD/BCH\n' + 'BCHBTC Price: ' + BCHBTCData.BTC + ' BCH/BTC\n' + 'ETHBTC Price: ' + ETHBTCData.BTC + ' ETH/BTC\n' + 'BCHETH Price: ' + BCHETHData.ETH + 'BCH/ETH';
     } catch (err) {
         console.log(err);
     }
